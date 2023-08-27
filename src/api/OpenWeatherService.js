e3af296e2d7800465a82b21c524179ba0c242ee8
Ukeme David Eseme <skywalkershadow9@gmail.com>
2023-08-27 16:50:54 +0000

    add environmental variables

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,8 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
+const WEATHER_API_KEY = process.env.REACT_APP_API_KEY;
+console.log(process.env.REACT_APP_API_KEY)
 
 const GEO_API_OPTIONS = {
   method: 'GET',
